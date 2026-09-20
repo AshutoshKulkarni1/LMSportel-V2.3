086c55c4239aeed2d8a9a71bf6b292c05035c863
AshutoshKulkarni1 <ashutoshgk23@gmail.com>
2026-09-20 14:02:53 +0530

    Material font stylesheet added

--- src/php/public/student/test-analysis.php
+++ src/php/public/student/test-analysis.php
@@ -259,7 +259,7 @@ $currentPage = 'test-analysis';
         href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap"
         rel="stylesheet"
     >
-
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20,300,0,0">
     <script src="https://unpkg.com/lucide@latest"></script>
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 </head>
